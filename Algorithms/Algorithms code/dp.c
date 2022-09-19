d6effce070e6d6bf30e69208097665842bfbc4bb
bratin03 <bratinmondal689@gmail.com>
2022-09-19 22:47:29 +0530

    dp

--- Algorithms/Algorithms code/dp.c
+++ Algorithms/Algorithms code/dp.c
@@ -75,3 +75,22 @@ int size_a=strlen(a),size_b=strlen(b);//length of string
   else 
    j--;
   }
+
+  //0 - 1 Knapsack Problem
+  /*You are given weights and values of N items, put these items in a knapsack of capacity W to get the maximum total value in the knapsack. Note that we have only one quantity of each item.
+In other words, given two integer arrays val[0..N-1] and wt[0..N-1] which represent values and weights associated with N items respectively. Also given an integer W which represents knapsack capacity, find out the maximum value subset of val[] such that sum of the weights of this subset is smaller than or equal to W. You cannot break an item, either pick the complete item or dont pick it (0-1 property)*/
+
+int knapSack(int W, int wt[], int val[], int n) 
+    { 
+       int dp[n+1][W+1];
+       for(int i=0;i<=n;i++)dp[i][0]=0;
+       for(int i=0;i<=W;i++)dp[0][i]=0;
+       for(int i=1;i<=n;i++)
+       {
+           for(int j=0;j<=W;j++)
+           {
+               if(wt[i-1]>j)dp[i][j]=dp[i-1][j];
+           
+           else dp[i][j]=max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
+       }}
+    return dp[n][W];}
