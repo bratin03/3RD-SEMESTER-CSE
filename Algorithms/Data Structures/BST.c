2ccb9dc236c128eaa54b685d56b32ef46a59ba97
bratin03 <bratinmondal689@gmail.com>
2022-10-02 16:03:50 +0530

    Data Structures

---
+++ Algorithms/Data Structures/BST.c
@@ -0,0 +1,112 @@
+int isBST(struct  node* root){
+    static struct node *prev = NULL;
+    if(root!=NULL){
+        if(!isBST(root->left)){
+            return 0;
+        }
+        if(prev!=NULL && root->data <= prev->data){
+            return 0;
+        }
+        prev = root;
+        return isBST(root->right);
+    }
+    else{
+        return 1;
+    }
+}
+
+struct node * search(struct node* root, int key){
+    if(root==NULL){
+        return NULL;
+    }
+    if(key==root->data){
+        return root;
+    }
+    else if(key<root->data){
+        return search(root->left, key);
+    }
+    else{
+        return search(root->right, key);
+    }
+}
+
+struct node * searchIter(struct node* root, int key){
+    while(root!=NULL){
+        if(key == root->data){
+            return root;
+        }
+        else if(key<root->data){
+            root = root->left;
+        }
+        else{
+            root = root->right;
+        }
+    }
+    return NULL;
+}
+
+void insert(struct node *root, int key){
+
+   if (root == NULL)
+       return createNode(key);
+       
+   struct node *prev = NULL;
+   while(root!=NULL){
+       prev = root;
+       if(key==root->data){
+           printf("Cannot insert %d, already in BST", key);
+           return;
+       }
+       else if(key<root->data){
+           root = root->left;
+       }
+       else{
+           root = root->right;
+       }
+   }
+   struct node* new = createNode(key);
+   if(key<prev->data){
+       prev->left = new;
+   }
+   else{
+       prev->right = new;
+   }
+}
+
+struct node *inOrderPredecessor(struct node* root){
+    root = root->left;
+    while (root->right!=NULL)
+    {
+        root = root->right;
+    }
+    return root;
+}
+
+struct node *deleteNode(struct node *root, int value){
+
+    struct node* iPre;
+    if (root == NULL){
+        return NULL;
+    }
+    if (root->left==NULL&&root->right==NULL&&root->data=value){
+        free(root);
+        return NULL;
+    }
+
+    //searching for the node to be deleted
+    if (value < root->data){
+        root-> left = deleteNode(root->left,value);
+    }
+    else if (value > root->data){
+        root-> right = deleteNode(root->right,value);
+    }
+    //deletion strategy when the node is found
+    else{
+        iPre = inOrderPredecessor(root);
+        root->data = iPre->data;
+        root->left = deleteNode(root->left, iPre->data);
+    }
+    return root;
+}
+
+
